785fb4b76c1da33345f28d1a1bcd471086744591
nixster <iamnixster@gmail.com>
2020-04-04 19:00:46 +0800

    added audio during thrust

--- 3_Project_Boost/Assets/Scripts/Rocket.cs
+++ 3_Project_Boost/Assets/Scripts/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         // This get component only acts on components of type rigid body
-        rigidBody = GetComponent<Rigidbody>();       
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -28,6 +30,14 @@ public class Rocket : MonoBehaviour
             // Thrusts while rotating
             print("Thrusting");
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
